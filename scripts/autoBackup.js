65c98244c693c66e22fd90c42f5b51925e3d71e4
RuoE <louie446725060@live.com>
2020-10-25 17:42:25 +0800

    Form auto backup script's commit

---
+++ scripts/autoBackup.js
@@ -0,0 +1,32 @@
+require('shelljs/global');
+
+try {
+	hexo.on('deployAfter', function() {//当deploy完成后执行备份
+		run();
+	});
+} catch (e) {
+	console.log("产生了一个错误<(￣3￣)> !，错误详情为：" + e.toString());
+}
+
+function run() {
+	if (!which('git')) {
+		echo('Sorry, this script requires git');
+		exit(1);
+	} else {
+		echo("======================Auto Backup Begin===========================");
+		cd('D:\RuoE.github.io');    //此处修改为Hexo根目录路径
+		if (exec('git add .').code !== 0) {
+			echo('Error: Git add failed');
+			exit(1);
+		}
+		if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
+			echo('Error: Git commit failed');
+			exit(1);
+		}
+		if (exec('git push').code !== 0) {
+			echo('Error: Git push failed');
+			exit(1);
+		}
+		echo("==================Auto Backup Complete============================")
+	}
+}
